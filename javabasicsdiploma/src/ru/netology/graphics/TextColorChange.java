da97d1a106563caf967fbbef5a8f42c6356d58ff
Sergei Rtischev <sergei1502@yandex.ru>
2021-10-12 07:13:44 +0500

    Конвертер v2

--- javabasicsdiploma/src/ru/netology/graphics/TextColorChange.java
+++ javabasicsdiploma/src/ru/netology/graphics/TextColorChange.java
@@ -5,27 +5,11 @@ import ru.netology.graphics.image.TextColorSchema;
 
 
 public class TextColorChange implements TextColorSchema {
+    public char[] symbol = {'#', '$', '@', '%', '*', '+', '-', ','};
 
-    char symbol = ' ';
 
     @Override
     public char convert(int color) {
-
-        if ((color >= 201) && (color <= 255)) {
-            symbol = '-';
-        }
-        if ((color >= 151) && (color <= 200)) {
-            symbol = '+';
-        }
-        if ((color >= 101) && (color <= 150)) {
-            symbol = '%';
-        }
-        if ((color >= 51) && (color <= 100)) {
-            symbol = '@';
-        }
-        if ((color >= 0) && (color <= 50)) {
-            symbol = '$';
-        }
-        return symbol;
+        return symbol[color / 32];
     }
 }
